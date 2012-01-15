9b38373795d4f3a80139dce8dbf72dc202c2b0cb
Mirela Budaes <mirelusbc@gmail.com>
2012-01-15 11:33:23 +0100

    Fixed projects and events in the homepage.

--- MindBus.Extensions/DocumentTypes/Home.cs
+++ MindBus.Extensions/DocumentTypes/Home.cs
@@ -6,5 +6,14 @@ namespace MindBus.Extensions.DocumentTypes
     [DocumentType(Name = "Home", IconUrl = "MB-home.png", Thumbnail = "docWithImage.png", AllowedTemplates = new[] { "Home" }, DefaultTemplate = typeof(Home))]
     public class Home : PageSettings
     {
+        [DocumentTypeProperty(UmbracoPropertyType.Numeric, Name = "Projects shown", Tab = "Content",
+           Description = "How many projecs to show.", DefaultValue = "2", Mandatory = false)]
+        public string ProjectsShown { get; set; }
+
+        [DocumentTypeProperty(UmbracoPropertyType.Numeric, Name = "Events shown", Tab = "Content", Description = "How many events to show.", Mandatory = false, DefaultValue = "2")]
+        public string EventsShown { get; set; }
+
+        [DocumentTypeProperty(UmbracoPropertyType.Other, OtherTypeName = "Event Type Picker", Name = "Type of events", Tab = "Content", Description = "", Mandatory = true)]
+        public string TypeOfEvents { get; set; }
     }
 }
